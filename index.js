f4131582617a39b62a2b610e3353ede5019afadf
Dominik Custovic <dcustovic@iolap.com>
2021-02-13 18:54:54 +0100

    creating a player

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+    
+    drawPlayer() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 4, Math.PI * 2, false);
+        ctx.fill();
+    }
+    
+}
+
+const domo = new Player(100, 100, 30, 'red');
+domo.drawPlayer();
