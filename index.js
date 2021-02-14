508e3dca896c0197fb2b0ab467428e14f991e653
Dominik Custovic <dcustovic@iolap.com>
2021-02-14 13:43:08 +0100

    creating a projectile that spawns in the center of the player

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ canvas.height = window.innerHeight;
 
 class Player {
     constructor(x, y, radius, color) {
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = color
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
     }
     
     drawPlayer() {
@@ -20,8 +20,36 @@ class Player {
     }
 }
 
-const x = canvas.width / 2;
-const y = canvas.height /2;
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
 
-const domo = new Player(x, y, 30, 'red');
+    drawProjecile() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const domo = new Player(x, y, 30, 'black');
 domo.drawPlayer();
+
+window.addEventListener('click', (event) => {
+    const metak = new Projectile(
+        x,
+        y,
+        5,
+        'lime',
+        null
+    );
+    metak.drawProjecile();
+})
